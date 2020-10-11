88ed87058b81951e0e24d02c809da8caf4b88f76
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-11 14:26:52 +0800

    Fancy header

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home(props) {
           textAlign: 'left',
           background: '#B4436C',
           backgroundImage:
-            "url(\"data:image/svg+xml,%3Csvg width='32' height='64' viewBox='0 0 32 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 28h20V16h-4v8H4V4h28v28h-4V8H8v12h4v-8h12v20H0v-4zm12 8h20v4H16v24H0v-4h12V36zm16 12h-4v12h8v4H20V44h12v12h-4v-8zM0 36h8v20H0v-4h4V40H0v-4z' fill='%23d44c7d' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E\")",
+            "url(\"data:image/svg+xml,%3Csvg width='32' height='64' viewBox='0 0 32 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 28h20V16h-4v8H4V4h28v28h-4V8H8v12h4v-8h12v20H0v-4zm12 8h20v4H16v24H0v-4h12V36zm16 12h-4v12h8v4H20V44h12v12h-4v-8zM0 36h8v20H0v-4h4V40H0v-4z' fill='%23d44c7d' fill-opacity='0.3' fill-rule='evenodd'/%3E%3C/svg%3E\"), linear-gradient(90deg, rgba(180,67,108,1) 0%, rgba(180,67,108,1) 47%, rgba(180,67,108,0.11388305322128855) 100%), url(\"data:image/svg+xml,%3Csvg width='32' height='64' viewBox='0 0 32 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 28h20V16h-4v8H4V4h28v28h-4V8H8v12h4v-8h12v20H0v-4zm12 8h20v4H16v24H0v-4h12V36zm16 12h-4v12h8v4H20V44h12v12h-4v-8zM0 36h8v20H0v-4h4V40H0v-4z' fill='%23d44c7d' fill-opacity='1' fill-rule='evenodd'/%3E%3C/svg%3E\")",
           marginTop: '0',
           padding: '50px',
           color: 'white'
@@ -37,7 +37,7 @@ export default function Home(props) {
         </Container>
       </Box>
       <Container sx={{ marginTop: '20px', marginBottom: '20px' }}>
-        <Grid columns={[null, null, 2, 3]} gap={3}>
+        <Grid columns={[null, null, 2, 3]} gap={4}>
           {props.posts.map(post => (
             <Link href={`/posts/${post.slug}`} sx={{ textDecoration: 'none' }}>
               <Card variant="interactiveALT">
@@ -45,7 +45,7 @@ export default function Home(props) {
                   src={post.image}
                   sx={{ width: '100%', height: '200px', objectFit: 'cover' }}
                 />
-                <Text sx={{ paddingLeft: '20px' }}>
+                <Text sx={{ paddingLeft: '20px', paddingRight: '20px' }}>
                   <Heading sx={{ marginTop: '10px', marginBottom: '12px' }}>
                     {post.title}
                   </Heading>
